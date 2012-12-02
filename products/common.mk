c418ac9385136402922d687a214bfc7260ea0adb
rascarlo <rascarlo@gmail.com>
2012-12-02 21:26:55 +0100

    bring back the aosp videoeditor

--- products/common.mk
+++ products/common.mk
@@ -46,8 +46,7 @@ PRODUCT_COPY_FILES += \
     vendor/rascarlo/proprietary/common/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
     vendor/rascarlo/proprietary/common/app/PartnerBookmarksProvider.apk:system/app/PartnerBookmarksProvider.apk \
     vendor/rascarlo/proprietary/common/app/Phonesky.apk:system/app/Phonesky.apk \
-    vendor/rascarlo/proprietary/common/app/Talk.apk:system/app/Talk.apk \
-    vendor/rascarlo/proprietary/common/app/VideoEditor.apk:system/app/VideoEditor.apk
+    vendor/rascarlo/proprietary/common/app/Talk.apk:system/app/Talk.apk
 
 # system/etc/permissions
 PRODUCT_COPY_FILES += \
