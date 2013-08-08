3489b21f7257c8f86d9be813daa4f2cd9f580722
rascarlo <rascarlo@gmail.com>
2013-08-08 17:00:17 +0200

    add grouper

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,5 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/rascarlo_emulator.mk \
+    $(LOCAL_DIR)/rascarlo_grouper.mk \
     $(LOCAL_DIR)/rascarlo_mako.mk \
     $(LOCAL_DIR)/rascarlo_maguro.mk \
     $(LOCAL_DIR)/rascarlo_manta.mk
---
+++ products/rascarlo_grouper.mk
@@ -0,0 +1,17 @@
+# Inherit AOSP device configuration for grouper.
+$(call inherit-product, device/asus/grouper/full_grouper.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/rascarlo/products/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := rascarlo_grouper
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := grouper
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.2.2/JDQ39/573038:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.2.2 JDQ39 573038 release-keys"
+
+# Inherit media effect blobs
+-include vendor/rascarlo/products/common_media_effects.mk
