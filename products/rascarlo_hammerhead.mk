b262d3ca5277dea0f0d41f1d63eebd05a7277aff
rascarlo <rascarlo@gmail.com>
2013-11-11 14:30:17 +0100

    hammerhead proper fingerprint

--- products/rascarlo_hammerhead.mk
+++ products/rascarlo_hammerhead.mk
@@ -11,7 +11,7 @@ PRODUCT_DEVICE := hammerhead
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="fingerprint: google/hammerhead/hammerhead:4.4/KRT16M/893803:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4 KRT16M 893803 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hammerhead BUILD_FINGERPRINT="google/hammerhead/hammerhead:4.4/KRT16M/893803:user/release-keys" PRIVATE_BUILD_DESC="hammerhead-user 4.4 KRT16M 893803 release-keys"
 
 # Build SimToolKit
 PRODUCT_PACKAGES += \
