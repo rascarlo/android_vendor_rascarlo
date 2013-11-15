ef3da4e245db2db7d30e1e5ee523decaf26d8858
rascarlo <rascarlo@gmail.com>
2013-11-15 11:23:21 +0100

    mako: fingerprint for android-4.4_r1.1 KRT160

--- products/rascarlo_mako.mk
+++ products/rascarlo_mako.mk
@@ -11,7 +11,7 @@ PRODUCT_DEVICE := mako
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
 
-#PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occam/mako:4.3/JWR66V/737497:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.3 JWR66V 737497 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occam/mako:4.4/KRT16O/893803:user/release-keys" PRIVATE_BUILD_DESC="occam-user 4.4 KRT16O 893803 release-keys"
 
 # Inherit media effect blobs
 -include vendor/rascarlo/products/common_media_effects.mk
