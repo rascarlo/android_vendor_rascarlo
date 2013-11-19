f195448a8d4a80472866943a4fc875449acc0c00
rascarlo <rascarlo@gmail.com>
2013-11-19 12:16:24 +0100

    add grouper

---
+++ products/rascarlo_grouper.mk
@@ -0,0 +1,23 @@
+# Inherit AOSP device configuration for grouper.
+$(call inherit-product, device/asus/grouper/full_grouper.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/rascarlo/products/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := rascarlo_grouper
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := grouper
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4/KRT16M/527662:user/release-keys"
+PRIVATE_BUILD_DESC="nakasi-user 4.4 KRT16M 527662 release-keys"
+
+# Inherit media effect blobs
+-include vendor/rascarlo/products/common_media_effects.mk
+
+# disable strictmode on boot
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.strictmode.visual=0 \
+    persist.sys.strictmode.disable=1
