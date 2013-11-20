4d0a4ca51a62652292396ca5e35fb6b919a468cc
rascarlo <rascarlo@gmail.com>
2013-11-20 13:32:08 +0100

    organize vendor/media imports

--- products/common_media_effects.mk
+++ products/common_media_effects.mk
@@ -2,8 +2,8 @@
 
 # media effects
 PRODUCT_COPY_FILES +=  \
-    vendor/rascarlo/proprietary/common/system/media/LMspeed_508.emd:system/media/LMspeed_508.emd \
-    vendor/rascarlo/proprietary/common/system/media/PFFprec_600.emd:system/media/PFFprec_600.emd
+    vendor/rascarlo/proprietary/common/vendor/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
+    vendor/rascarlo/proprietary/common/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
 
 # video files
 $(call inherit-product-if-exists, frameworks/base/data/videos/VideoPackage2.mk)
