a17f55cd1e7110ced21ca498c15fb84a859ce919
rascarlo <rascarlo@gmail.com>
2013-11-22 08:09:34 +0100

    drop hammerhead

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,4 @@
 add_lunch_combo rascarlo_emulator-eng
 add_lunch_combo rascarlo_grouper-eng
-add_lunch_combo rascarlo_hammerhead-user
 add_lunch_combo rascarlo_mako-user
 add_lunch_combo rascarlo_manta-user
