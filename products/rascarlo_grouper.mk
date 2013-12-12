cc32a3d169376f1225efdeed0e1ea9c3fbdfaa3f
rascarlo <rascarlo@gmail.com>
2013-12-12 11:15:18 +0100

    grouper: fingerprint for android-4.4.2_r1 KOT49H

--- products/rascarlo_grouper.mk
+++ products/rascarlo_grouper.mk
@@ -11,8 +11,9 @@ PRODUCT_DEVICE := grouper
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.4/KRT16M/527662:user/release-keys"
-PRIVATE_BUILD_DESC="nakasi-user 4.4 KRT16M 527662 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.4.2/KOT49H/937116:user/release-keys \
+    PRIVATE_BUILD_DESC="nakasi-user 4.4.2 KOT49H 937116 release-keys"
 
 # Inherit media effect blobs
 -include vendor/rascarlo/products/common_media_effects.mk
