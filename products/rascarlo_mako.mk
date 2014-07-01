cb90fa98f4178601c1484942f1c6dffc6cc4cedc
rascarlo <rascarlo@gmail.com>
2014-07-01 14:50:39 +0200

    mako 4.4.4 - KTU84P

--- products/rascarlo_mako.mk
+++ products/rascarlo_mako.mk
@@ -14,8 +14,8 @@ PRODUCT_MANUFACTURER := LGE
 # FIngerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
-    BUILD_FINGERPRINT=google/occam/mako:4.4.3/KTU84L/1148727:user/release-keys \
-    PRIVATE_BUILD_DESC="occam-user 4.4.3 KTU84L 1148727 release-keys"
+    BUILD_FINGERPRINT=google/occam/mako:4.4.4/KTU84P/1227136:user/release-keys \
+    PRIVATE_BUILD_DESC="occam-user 4.4.4 KTU84P 1227136 release-keys"
 
 # Inherit media effect blobs
 -include vendor/rascarlo/products/common_media_effects.mk
