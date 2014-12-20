22339569235f663916d32928c94fb0cc55626afd
rascarlo <rascarlo@gmail.com>
2014-12-20 15:56:43 +0100

    grouper: 5.0.2 - LRX22G

--- products/rascarlo_grouper.mk
+++ products/rascarlo_grouper.mk
@@ -14,8 +14,8 @@ PRODUCT_MANUFACTURER := asus
 #Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=nakasi \
-    BUILD_FINGERPRINT=google/nakasi/grouper:5.0/LRX21P/1570855:user/release-keys \
-    PRIVATE_BUILD_DESC="nakasi-user 5.0 LRX21P 1570855 release-keys"
+    BUILD_FINGERPRINT=google/nakasi/grouper:5.0.2/LRX22G/1649326:user/release-keys \
+    PRIVATE_BUILD_DESC="nakasi-user 5.0.2 LRX22G 1649326 release-keys"
 
 # Inherit media effect blobs
 -include vendor/rascarlo/products/common_media_effects.mk
