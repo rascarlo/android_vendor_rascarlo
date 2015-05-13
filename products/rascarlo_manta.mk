8c820c35185ca265332f16a0d5222a4b7cbaf3e9
rascarlo <rascarlo@gmail.com>
2015-05-13 10:05:06 +0200

    manta: 5.1.1 - LMY47V

--- products/rascarlo_manta.mk
+++ products/rascarlo_manta.mk
@@ -14,8 +14,8 @@ PRODUCT_MANUFACTURER := samsung
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=mantaray \
-    BUILD_FINGERPRINT=google/mantaray/manta:5.1/LMY47D/1743759:user/release-keys \
-    PRIVATE_BUILD_DESC="mantaray-user 5.1 LMY47D 1743759 release-keys"
+    BUILD_FINGERPRINT=google/mantaray/manta:5.1.1/LMY47V/1836172:user/release-keys \
+    PRIVATE_BUILD_DESC="mantaray-user 5.1.1 LMY47V 1836172 release-keys"
 
 # Inherit media effect blobs
 -include vendor/rascarlo/products/common_media_effects.mk
