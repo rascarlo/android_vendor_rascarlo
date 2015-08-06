6bd1c87b55010c098e02f4e2fb7313e4d25e0cd1
rascarlo <rascarlo@gmail.com>
2015-08-06 15:40:56 +0200

    hammerhead 5.1.1 - LMY48I

--- products/rascarlo_hammerhead.mk
+++ products/rascarlo_hammerhead.mk
@@ -14,8 +14,8 @@ PRODUCT_MANUFACTURER := LGE
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
-    BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1.1/LMY48B/1863243:user/release-keys \
-    PRIVATE_BUILD_DESC="hammerhead-user 5.1.1 LMY48B 1863243 release-keys"
+    BUILD_FINGERPRINT=google/hammerhead/hammerhead:5.1.1/LMY48I/2074855:user/release-keys \
+    PRIVATE_BUILD_DESC="hammerhead-user 5.1.1 LMY48I 2074855 release-keys"
 
 # Inherit media effect blobs
 -include vendor/rascarlo/products/common_media_effects.mk
