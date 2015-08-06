bbe0e31899de265c5f367d9ec79b1b1e2d7fe037
rascarlo <rascarlo@gmail.com>
2015-08-06 18:06:19 +0200

    mako: 5.1.1 - LMY48I

--- products/rascarlo_mako.mk
+++ products/rascarlo_mako.mk
@@ -14,8 +14,8 @@ PRODUCT_MANUFACTURER := LGE
 # FIngerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
-    BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY47V/1836172:user/release-keys \
-    PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY47V 1836172 release-keys"
+    BUILD_FINGERPRINT=google/occam/mako:5.1.1/LMY48I/2074855:user/release-keys \
+    PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48I 2074855 release-keys"
 
 # Inherit media effect blobs
 -include vendor/rascarlo/products/common_media_effects.mk
